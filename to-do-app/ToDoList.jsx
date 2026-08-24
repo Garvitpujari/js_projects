7a08241715ed8e4c440fd00cd59881524568adf2
Garvitpujari <garvitpujari98@gmail.com>
2026-08-24 23:47:32 +0530

    Refactor ToDoList component with task management

--- to-do-app/ToDoList.jsx
+++ to-do-app/ToDoList.jsx
@@ -1,66 +1,58 @@
-import {useState} from 'react';
+import {useState} from 'react'
 function ToDoList(){
 
-    const[tasks,setTasks]=useState([]);
+    const[tasks,setTasks]=useState(["You are good","It's great to have u on board"]);
     const[newTask,setNewTask]=useState("");
 
     function handleInputChange(event){
         setNewTask(event.target.value);
 
     }
+    function addTask(){
+        if(newTask.trim()!==""){
+            setTasks(t=>[...t,newTask]);
+            setNewTask("");
+        }
 
-    function deleteTask(index){
-        const updatedTasks=tasks.filter((_,i)=> index!==i);
+    }
+    function removeTask(index){
+        const updatedTasks=tasks.filter((_,i)=>i!==index);
         setTasks(updatedTasks);
 
     }
-    function addTask(){
-        if(newTask.trim()!==""){
-        setTasks(t=> [...t,newTask]);
-        setNewTask("");
-        }
-    
 
-    }
     function moveTaskUp(index){
-
         if(index>0){
             const updatedTasks=[...tasks];
-            [updatedTasks[index],updatedTasks[index-1]]=[updatedTasks[index-1],updatedTasks[index]];
-            setTasks(updatedTasks);
+            [updatedTasks[index],updatedTasks[index-1]]=[updatedTasks[index-1],updatedTasks[index]]
+             setTasks(updatedTasks);
         }
 
     }
     function moveTaskDown(index){
-        if(index<tasks.length-1){
+            if(index<tasks.length-1){
             const updatedTasks=[...tasks];
-            [updatedTasks[index],updatedTasks[index+1]]=[updatedTasks[index+1],updatedTasks[index]];
-            setTasks(updatedTasks);
+            [updatedTasks[index],updatedTasks[index+1]]=[updatedTasks[index+1],updatedTasks[index]]
+             setTasks(updatedTasks);
         }
-
-        
-    }
-
  
+    }
+    
 
-    return (
+
+    return(
         <div className="main-container">
             <div className="header">
-            <p>To Do List</p>
+                <p>To Do List</p>
             </div>
-           <div className="input">
-            <input type="text" id="task" placeholder="Enter your task" value={newTask} onChange={handleInputChange}>
-            </input>
+            <input type="text" id="task" placeholder="Enter your task" value={newTask} onChange={handleInputChange}></input>
+            <button className="add-button" onClick={addTask}>Add</button>
 
-            <button className="add-button" onClick={addTask}>Add</button>        
-             
-           </div>
-
-           <ol>
-            {tasks.map((task,index)=>
+            <ol>
+                   {tasks.map((task,index)=>
             <li key={index}>
                 <span className="text">{task}</span>
-                <button className="delete-button" onClick={()=> deleteTask(index)}>delete</button>
+                <button className="delete-button" onClick={()=> removeTask(index)}>delete</button>
                   <button className="move-task" onClick={()=>  moveTaskUp(index)}>up</button>  
                     <button className="move-task" onClick={()=>  moveTaskDown(index)}>down</button>  
                     
@@ -68,10 +60,12 @@ function ToDoList(){
             </li>
             
             )}
-           </ol>
+            </ol>
+
 
         </div>
+       
     )
 
 }
-export default ToDoList;
+export default ToDoList; 
